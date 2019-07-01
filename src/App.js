bf57ae10300fdba5b67f39e911eb2d1da4ba1d0d
natuShubham96 <shubhamnatu52@gmail.com>
2019-07-01 19:58:38 +0530

    NavBar Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import Movies from "./Movies";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NavBar from "./components/common/navBar";
+import NotFound from "./components/common/notFound";
 
 class App extends Component {
   render() {
-    return <Movies />;
+    return (
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/rentals" component={Rentals} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </div>
+    );
   }
 }
 
