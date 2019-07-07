eb1b008ae4c0858d5cd4ba6f15972b05fafe7302
natu.shubham96 <shubhamnatu52@gmail.com>
2019-07-07 12:25:47 +0530

    Disabling login button

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -112,7 +112,9 @@ class LoginForm extends Component {
             aria="passwordHelp"
             error={this.state.errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
